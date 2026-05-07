8ef9655123102fad6f86339791ced54e3683d56d
Dinuka991028 <dinuka.dhananjaya.silva@gmail.com>
2026-05-07 17:53:43 +0530

    update

---
+++ src/InsertionSort.java
@@ -0,0 +1,36 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 6, 2, 3, 1, 8, 4};
+
+        System.out.println("Before Sorting:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+        System.out.println("--------------------");
+
+        // Insertion Sort
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        System.out.println("After Sorting:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
